0bb81cf882cb451d4f19467d9b822e52e7019709
Danny2117 <dannyabebe66@gmail.com>
2022-06-05 04:01:52 -0700

    tas

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main -main block
+ * return: 0
+ */
+int main(void)
+{
+char strl[] = "\"Programming is like building a multilingual puzzle";
+puts(strl);
+return (0);
+}

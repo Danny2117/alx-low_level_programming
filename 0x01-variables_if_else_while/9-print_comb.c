ea6cab1dba6056a7dab3773f0602d0797916af11
Danny2117 <dannyabebe66@gmail.com>
2022-06-06 13:54:55 -0700

    ta99

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- * main - Prints all the numbers of base 16 in lowercase.
- *
- * Return: Always 0.
- *
+ * main - Prints numbers between 0 to 9 with commas and spaces.
+ * am from ethiopia
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int num;
-char letter;
-for (num = 0; num < 10; num++)
-putchar((num % 10) + '0');
-for (letter = 'a'; letter <= 'f'; letter++)
-putchar(letter);
+int i;
+for (i = 48; i < 58; i++)
+{
 putchar(i);
 if (i != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
 putchar('\n');
 return (0);
 }

1a4258a0b9ae45b8cedf6427e64ff158d3abb67e
Danny2117 <dannyabebe66@gmail.com>
2022-06-08 14:37:29 -0700

    sds

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,9 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-#include <stdio.h>
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+#ifndef MAIN_H
+#define MAINH_H
+#include<stdio.h>
+void positive_or_negative(int i):
+int largest_number(int a,int b,int c);
+void print_remaining_days(int month,int day,int year);
+int convert_day(int month,int day);
+void print times_table(int n);
 #endif

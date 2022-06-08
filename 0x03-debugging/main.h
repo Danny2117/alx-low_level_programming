3558df82ae8b99e6e31dab2256a4ec3c690115d0
Danny2117 <dannyabebe66@gmail.com>
2022-06-08 14:17:29 -0700

    debeg0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include<stdio.h>
+void positive_or_negative(inti);
+int largest number(inta,intb,intc);
+void print remaining_days(int month,int day,int year);
+int convert_day(int month,int day);
+void print times table(intn);
+#endiff

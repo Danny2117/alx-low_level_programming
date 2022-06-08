e98717e432c11f4c85519de28f4013fca114119e
Danny2117 <dannyabebe66@gmail.com>
2022-06-08 12:49:30 -0700

    104ts

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,42 +1,43 @@
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *        1 and 2, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-int co;
-unsigned long fb1 = 0, fb2 = 1, sum;
-unsigned long i, j, k, m;
-unsigned long x, y;
-for (co = 0; co < 92; co++)
+int count;
+unsigned long fib1 = 0, fib2 = 1, sum;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+unsigned long half1, half2;
+for (count = 0; count < 92; count++)
 {
-sum = fb1 + fb2;
+sum = fib1 + fib2;
 printf("%lu, ", sum);
-fb1 = fb2;
-fb2 = sum;
+fib1 = fib2;
+fib2 = sum;
 }
-i = fb1 / 10000000000;
-k = fb2 / 10000000000;
-j = fb1 % 10000000000;
-m = fb2 % 10000000000;
-for (co = 93; co < 99; co++)
+fib1_half1 = fib1 / 10000000000;
+fib1_half1 = fib2 / 10000000000;
+fib1_half1 = fib1 % 10000000000;
+fib1_half1 = fib2 % 10000000000;
+for (count = 93; count < 99; count++)
 {
-x = i + k;
-y = j + m;
-if (j + m > 9999999999)
+half1 = fib1_half1 + fib2_half1;
+half2 = fib1_half2 + fib2_half2;
+if (fib1_half2 + fib2_half2 > 9999999999)
 {
-x = x + 1;
-y = y % 10000000000;
+half1 += 1;
+half2 %= 10000000000;
 }
-printf("%lu%lu", x, y);
-if (co != 98)
-printf(",");
-i = k;
-j = m;
-k = x;
-m = y;
+printf("%lu%lu", half1, half2);
+if (count != 98)
+printf(", ");
+fib1_half1 = fib2_half1;
+fib1_half2 = fib2_half2;
+fib2_half1 = half1;
+fib2_half2 = half2;
 }
 printf("\n");
 return (0);

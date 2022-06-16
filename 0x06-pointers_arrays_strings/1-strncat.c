b92bde3a1a7cdcead2f36b5f424058de3a4a4926
Danny2117 <dannyabebe66@gmail.com>
2022-06-16 04:06:41 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strncat - concats two arrays
+ *_strncat - concats two arrays
  *
  * @dest: destination of concat
  * @src: source array to concat
@@ -11,18 +11,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-  int i = 0;
-  int j;
-
-  while (dest[i] != '\0')
-    {
-      i++;
-    }
-  for (j = 0; j < n && src[j] != '\0'; j++)
-    {
-      dest[i] = src[j];
-      i++;
-    }
-  return (dest);  
+int i = 0;
+int j;
+while (dest[i] != '\0')
+{
+i++;
 }
-
+for (j = 0; j < n && src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+}
+return (dest);
+}

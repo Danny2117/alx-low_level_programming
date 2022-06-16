7cff499a68598dad77bc6cc3357aad7bcb7d9a30
Danny2117 <dannyabebe66@gmail.com>
2022-06-16 05:48:04 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,30 @@
-int check_seperators(char c);
-
+#include "main.h"
 /**
- * cap_string - a function that capitalizes all words of a string.
- * @s: An input string to capitalize letters
- * Return: pointer to s
+ * cap_string - function that capitalizes all words of a string.
+ * @s: string
+ * Return: output
  */
-
 char *cap_string(char *s)
 {
 	int i = 0;
 
-	while (s[i])
+	if (s[i] >= 97 && s[i] <= 122)
 	{
-		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-			s[i] -= 32;
-		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-			s[i + 1] -= 32;
+		s[i] -= 32;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'
+				|| s[i] == ',' || s[i] == ';' || s[i] == '.'
+				|| s[i] == '!' || s[i] == '?' || s[i] == '"'
+				|| s[i] == '(' || s[i] == ')' || s[i] == '{'
+				|| s[i] == '}')
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+			{
+				s[i + 1] -= 32;
+			}
+		}
 		i++;
 	}
 	return (s);

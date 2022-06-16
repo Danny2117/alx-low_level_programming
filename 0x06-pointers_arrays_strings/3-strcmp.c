38f6b65543d0cde10baf6e9cdd5639df397bbfcf
Danny2117 <dannyabebe66@gmail.com>
2022-06-16 04:18:57 -0700

    task345

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,12 +6,10 @@
  *
  * Return: string value
  */
-
 int _strcmp(char *s1, char *s2)
 {
-  int i = 0;
-
-  while (s1[i] == s2[i] && s1[i] != '\0')
-    i++;
-  return (s1[i] - s2[i]); 
+int i = 0;
+while (s1[i] == s2[i] && s1[i] != '\0')
+i++;
+return (s1[i] - s2[i]);
 }
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,13 +7,12 @@
  */
 void reverse_array(int *a, int n)
 {
-  int i;
-  int r;
-
-  for (i = 0; i < n / 2; i++)
-    {
-      r = a[i];
-      a[i] = a[n - i - 1];
-      a[n - i - 1] = r;
-    }
+int i;
+int r;
+for (i = 0; i < n / 2; i++)
+{
+r = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = r;
+}
 }
